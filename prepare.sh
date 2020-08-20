08293221a7c782a57b1761c275287a09608c6453
Florian Schnell <florian.schnell@netlight.com>
2020-08-20 16:58:46 +0200

    smaller updates

--- prepare.sh
+++ prepare.sh
@@ -15,7 +15,7 @@ echo "done."
 echo
 
 # get latest osm data
-echo "downloading latest openstreetmaps data ..."
+echo "downloading latest OSM data for Munich ..."
 if [ ! -f ./geo/oberbayern-latest.osm.pbf ]; then
   curl --insecure https://download.geofabrik.de/europe/germany/bayern/oberbayern-latest.osm.pbf -o ./geo/oberbayern-latest.osm.pbf
 else
@@ -25,7 +25,7 @@ echo "done."
 echo
 
 # get latest annotations
-echo "downloading latest munichways ratings ..."
+echo "downloading latest munichways.com ratings ..."
 if [ ! -f ./geo/munichways.json ]; then
   curl --insecure "https://mw.mhcmuc.de/cgi-bin/qgis_mapserv.fcgi?SERVICE=WFS&VERSION=2.0.0&REQUEST=GetFeature&TYPENAME=Radvorrangnetz%2CRadvorrangnetz_gesamt%2Centlang_Hauptstrassen%2CGesamtnetz&outputFormat=application/json" -o geo/munichways.json
   sed -i -e 's/"Name"/"name"/g' geo/munichways.json
@@ -35,10 +35,8 @@ fi
 echo "done."
 echo
 
-exit 1
-
 # create out.pbf and out.geojson
-echo "integrating ratings into OpenStreetMap data ..."
+echo "integrating ratings into OSM data ..."
 python3 annotate_osm.py
 echo "done."
 echo
