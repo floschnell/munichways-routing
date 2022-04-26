8301f4e6a724d16a3246473aff8bd881245c069e
Florian Schnell <floschnell@gmail.com>
2022-04-26 12:29:53 +0200

    update container name

--- serve.sh
+++ serve.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 trap 'docker stop radlnavi-frontend radlnavi-backend' INT
 docker run --rm -d -p 5000:5000 --name radlnavi-backend -v $(pwd):/data osrm/osrm-backend osrm-routed --algorithm mld /data/out/out.osrm
-docker run --env OSRM_BACKEND=http://51.15.61.199:5000 --rm -d -p 9966:9966 --name osrm-frontend osrm/osrm-frontend
+docker run --env OSRM_BACKEND=http://51.15.61.199:5000 --rm -d -p 9966:9966 --name radlnavi-frontend osrm/osrm-frontend
 wait
