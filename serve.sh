bdb548d23f1ba07c2e783b2eac7cd92dd868b804
Florian Schnell <floschnell@gmail.com>
2022-04-26 12:26:35 +0200

    run original osrm frontend

--- serve.sh
+++ serve.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-trap 'docker stop osrm-frontend osrm-backend' INT
-docker run --rm -d -p 5000:5000 --name osrm-backend -v $(pwd):/data osrm/osrm-backend osrm-routed --algorithm mld /data/out/out.osrm
-docker run --env PORT=9966 --rm -d -p 9966:9966 --name osrm-frontend floschnell/radlnavi-frontend
+trap 'docker stop radlnavi-frontend radlnavi-backend' INT
+docker run --rm -d -p 5000:5000 --name radlnavi-backend -v $(pwd):/data osrm/osrm-backend osrm-routed --algorithm mld /data/out/out.osrm
+docker run --env OSRM_BACKEND=http://51.15.61.199:9966 --rm -d -p 9966:9966 --name osrm-frontend osrm/osrm-frontend
 wait
