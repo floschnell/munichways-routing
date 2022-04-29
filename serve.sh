29164b11865b9bdb7f02d57d70208a6b82f2d2c3
Florian Schnell <floschnell@gmail.com>
2022-04-29 10:59:40 +0200

    update frontend image

--- serve.sh
+++ serve.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop radlnavi-frontend radlnavi-backend
 docker run --rm -d -p 5000:5000 --name radlnavi-backend -v $(pwd):/data osrm/osrm-backend osrm-routed --algorithm mld /data/out/out.osrm
-docker run -v $(pwd)/leaflet_options.js:/src/src/leaflet_options.js --rm -d -p 9966:9966 --name radlnavi-frontend osrm/osrm-frontend
+docker run -v $(pwd)/leaflet_options.js:/src/src/leaflet_options.js --rm -d -p 9966:9966 --name radlnavi-frontend floschnell/radlnavi-frontend
